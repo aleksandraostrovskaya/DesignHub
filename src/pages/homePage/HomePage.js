d2403d33a8abb5bdbed9a65cf10ea213244abf27
Aleksandra <118135737+aleksandraostrovskaya@users.noreply.github.com>
2024-09-14 18:37:01 +0300

    "homepage" line removed for correct operation and path changed for getting json

--- src/pages/homePage/HomePage.js
+++ src/pages/homePage/HomePage.js
@@ -22,7 +22,7 @@ const HomePage = () => {
 
   const fetchProjects = async () => {
     try {
-      const response = await fetch('/DesignHub/projects.json');
+      const response = await fetch('/projects.json');
       if (!response.ok) {
         throw new Error('Failed to fetch projects');
       }
